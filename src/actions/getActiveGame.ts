5dc2bdfcd643ed5d89ca824d4005f263c9e347a9
Gal Podlipnik <69755435+galpodlipnik1@users.noreply.github.com>
2023-08-02 02:16:42 +0200

    table wip

--- src/actions/getActiveGame.ts
+++ src/actions/getActiveGame.ts
@@ -4,7 +4,7 @@ import prisma from '@/lib/prismadb';
 import getCurrentUser from './getCurrentUser';
 const getActiveGame = async () => {
   const user = await getCurrentUser();
-
+  
   if (!user) {
     return null;
   }
@@ -15,7 +15,17 @@ const getActiveGame = async () => {
         ownerId: user.id,
         status: 'active',
       },
+      include: {
+        scores: {
+          select: {
+            id: true,
+            playerName: true,
+            score: true,
+          }
+        }
+      }
     });
+    
     return game;
   } catch (error: any) {
     throw new Error(error);
