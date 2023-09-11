d6da88fec24f046bb01a2b5b913ad21c179264ea
galpodlipnik1 <itzgal2004@gmail.com>
2023-09-11 23:00:01 +0200

    working leaderboard

---
+++ src/app/leaderboard/components/displayBoard.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import { GameInfo } from '@/types';
+import getActiveGame from '@/actions/getActiveGame';
+
+const DisplayBoard = () => {
+  const [gameInfo, setGameInfo] = useState<any>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getActiveGame();
+      setGameInfo(data);
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log(gameInfo);
+  }, [gameInfo]);
+
+  return (
+    <div className='flex flex-col w-1/2'>
+      <div className='flex flex-row justify-between'>
+        {gameInfo?.players.map((player, index) => {
+          return (
+            <div key={player} className='w-1/2 flex items-center flex-col'>
+              <div className='text-2xl font-bold text-pallete4'>
+                {player}
+              </div>
+              <hr className='w-full border-pallete4' />
+                {gameInfo?.scores.map((scoreObj) => {
+                  if(scoreObj.playerName === player) {
+                    return (
+                      <div key={scoreObj.playerName} className=' w-full flex flex-col items-center text-pallete4'>
+                        {scoreObj.score.map(score => {
+                          return (
+                            <div key={score} className='w-full flex items-center flex-col'>
+                              {score}
+                              <hr className='w-full border-pallete4' />
+                            </div>
+                          )
+                        })}
+                      </div>
+                    )
+                  }
+                })}
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default DisplayBoard
---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+
+interface Score {
+  id: string;
+  playerName: string;
+  score: number;
+}
+
+export interface GameInfo {
+  id: string;
+  ownerId: string;
+  name: string;
+  players: string[];
+  status: string;
+  createdAt: Date;
+  scores: Score[];
+}
